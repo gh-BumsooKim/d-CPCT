d0cf4e4af7818b5f5a295c6a376dbece5661cc84
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2022-03-18 23:54:23 +0900

    updated v0.0.6 : main version
    **Done**
    - Azure Kinect Calibration
     
    **ToDo**
    - Depth Transformation using Homography

--- code/calibration.py
+++ code/calibration.py
@@ -15,6 +15,8 @@ import cv2
 import numpy as np
 import open3d as o3d # for MS Azure Kinect
 
+global ctn
+
 def print_info(msg: str, **kwargs) -> None:
     """
     msg :
@@ -49,122 +51,119 @@ def calibration(args: argparse.ArgumentParser) -> None:
     
     STATUS = 'VIEW' # To be replaced to argparse
     
-    if STATUS.upper() == 'VIEW':
-        cap = cv2.VideoCapture(1)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    
+    # ----------------------------------------------------------------------- #
+    
+    
+    ### 0. Camera Connection and Window Settings
+    ## 0-1. Kinect Config
+    kinect = o3d.io.AzureKinectSensor(o3d.io.AzureKinectSensorConfig())
+    
+    if not kinect.connect(0):
+        raise RuntimeError("[FAIL] Failed to Connection to Sensor")
+    
+    print_info("[INFO] Connected to Sensor")
+    
+    ## 0-2. Window Properties
+    #screen_num = len(sinfo.get_monitors())
+    screen_x, screen_y = sinfo.get_monitors()[-1].x, sinfo.get_monitors()[-1].y
+    cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+    cv2.moveWindow("window", screen_x, screen_y)
+    cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    print_info("[INFO] Created Window")
+    
+    
+    # ----------------------------------------------------------------------- #
+    
+    
+    ### 1. Operating Mode
+    ## 1-1. Camera View Mode 
+    if STATUS.upper() == 'VIEWe':
         
-        while True:            
-            ret, frame = cap.read()
-            
-            if ret == False:
-                print_info("[FAIL] Camera Connection Failed", info='fail')
-                break
-            
-            binary = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, binary = cv2.threshold(binary, 127, 255, cv2.THRESH_BINARY_INV)
-            
-            contour, hrc = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            #print(len(contour), hrc)
-            #for i, cont in enumerate(contour):
-            cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
-        
-            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-            cv2.moveWindow("window", 0, 0)
-            cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        while True:
+            rgbd = kinect.capture_frame(True)
+
+            if rgbd == None:
+                continue
+
+            frame = np.asarray(rgbd.color)[:,:,::-1]
             cv2.imshow("window", frame)
                         
             if cv2.waitKey(33) & 0xFF == ord('q'):
                 print_info("[TERMINATION] with Keyboard Interrupt")
-                cv2.destroyAllWindows(), cap.release()
+                cv2.destroyAllWindows()#, cap.release()
                 break
+            
+    ## 1-2. Calibration Mode        
     else:
-        #kinect = o3d.io.AzureKinectSensor(o3d.io.AzureKinectSensorConfig())
         
-        #if not kinect.connect(0):
-        #    raise RuntimeError("[FAIL] Failed to connect to sensor")
-        
-        crt = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-        
-        # 2. Webcam
-        
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-        print_info("[INFO] Created Window")
-        
-        #screen_num = len(sinfo.get_monitors())
-        screen_x, screen_y = sinfo.get_monitors()[-1].x, sinfo.get_monitors()[-1].y
-        cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-        cv2.moveWindow("window", screen_x, screen_y)
-        cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        print_info("[INFO] Set Window")
-        
-        error = 10.0
-        
-        ## 1-Way Projection
-        # Image Generation
+        # 2-1. 1-Way Projection
         u = ctypes.windll.user32
         window_w, window_h = u.GetSystemMetrics(0), u.GetSystemMetrics(1)                
-        one_way_image = np.zeros((window_h, window_w, 3))  
-        cv2.rectangle(one_way_image, (0, 0), (window_w, window_h),
-                      color=(0, 255, 0), thickness=50)        
+        one_way_image = np.ones((window_h, window_w, 3))* 255  
+        #cv2.rectangle(one_way_image, (0, 0), (window_w, window_h),
+        #              color=(0, 255, 0), thickness=50)
         
-        # Image Projection
         cv2.imshow("window", one_way_image)
-        cv2.waitKey(33)
-        cv2.imwrite("calibration/1-Way_Projection.jpg", one_way_image)
-        print_info("[INFO] 1-Way Proje1ction, and Synchronization")
-        time.sleep(2)
-        print_info("[INFO] 1-way Sleep Down")
-        ##
+        cv2.waitKey(1)
+        cv2.imwrite("0_1-way.jpg", one_way_image)
         
-        ## ToDo Image Plot // 22.02.23
+        # 2-2. 2-Way Capture
+        crt = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        time.sleep(2) # Connection Ready
+        rgbd = kinect.capture_frame(True)
         
-        ## 2-Way Capture         
-        ret, frame = cap.read()
-        if ret == False:
-            print_info("[FAIL] Camera Connection Failed", info='fail')
-            raise RuntimeError("[ERROR TERMINATION]")
+        if rgbd == None:
+            print("[WARNING] kincet is not connected")
+            pass
         
-        two_way_image = frame
-        cv2.imwrite("calibration/2-Way_Capture.jpg", two_way_image)
-        print_info("[INFO] 2-way Captured, and Synchronization")
-        time.sleep(2)
-        print_info("[INFO] 2-way Sleep Down")   
+        print_info("[INFO] 2-Way Capture")
+        two_way_image = np.asarray(rgbd.color)[:,:,::-1]
+        cv2.imwrite("0_2-Way.jpg", two_way_image)
         
-        # 3-way Projection
-        ret, frame = cap.read()
-        cv2.imwrite("calibration/3-way.jpg", frame)
-        print_info("[INFO] Done")
         
-        cap.release()
-        cv2.destroyAllWindows()
-        print_info("[TERMINATION]")
+        # 2-3. 3-Way Projection
+        three_way_image = two_way_image.copy()
+        grey = cv2.cvtColor(three_way_image, cv2.COLOR_BGR2GRAY)
+        ret, binary = cv2.threshold(grey, 137, 255, cv2.THRESH_BINARY_INV)
         
-        # Calibration
+        binary = cv2.blur(binary, (5, 5))
+        binary = cv2.dilate(binary, np.ones((5, 5)))
         
-        #image = cv2.imread("calibration/3-way.jpg")
-        #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        #ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+        cv2.imwrite("binary.jpg", binary)
         
-        #if ret == True:
-        #    corner = cv2.cornerSubPix(gray, corners, (9, 6), (-1, -1), crt)
-        #    frame = cv2.drawChessboardCorners(frame, (9, 6), corner, ret)
+        contour, hrc = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #print(len(contour), hrc)
+        #for i, cont in enumerate(contour):
         
-        """
-        while error > 1.0:
-            # Image Input
-            rgbd = kinect.capture_frame(0)    
-            if rgbd is None:
-                continue
+        #cv2.drawContours(three_way_image, contour, -1, (0, 0, 255), 3)
+    
+        #print("ddddddddddddd", contour)
+        global ctn
+        ctn = contour
+        
+        rect = cv2.minAreaRect(ctn[1])
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        print("Point", box)
+        cv2.drawContours(three_way_image, [box], 0, (0,255,0), 3)
+        
+        # Homography
+        # [Top-Left], [Bottom-Left], [Bottom-Right], [Top-Right]
+        point_src = box
+        point_dst = [[0,0],[1920,0],[],[]]
+        
+        # 22-03-18
+        # ToDo : Image Transformation
+        #
+        
+        cv2.imshow("window", three_way_image)
+
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            print_info("[TERMINATION] with Keyboard Interrupt")
+            cv2.destroyAllWindows()#, cap.release()
             
-            # 2. Webcam
-            ret, frame = cap.read()
-        
-            sd.Beep(2000, 100)
-            # Calibration using ChessBoard
-        """
+        cv2.imwrite("0_3-way.jpg", three_way_image)
         
         # Make Json Config File
         pass
